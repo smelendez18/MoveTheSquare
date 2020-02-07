539f793b2c9a16b8ff6551ef287635357e555e58
Shae Melendez <smelendez@ycp.edu>
2020-02-07 17:09:45 -0500

    red red rfed red red red red

--- MoveTheSquare/src/edu/ycp/cs320/movethesquare/ui/GameView.java
+++ MoveTheSquare/src/edu/ycp/cs320/movethesquare/ui/GameView.java
@@ -66,7 +66,7 @@ public class GameView extends JPanel {
 		
 		// djh2-KEC110-21: changed from GREEN to RED
 		// djh2-YCPlaptop: change from RED to YELLOW
-		g.setColor(Color.BLACK);
+		g.setColor(Color.RED);
 
 		Square square = model.getSquare();
 		
